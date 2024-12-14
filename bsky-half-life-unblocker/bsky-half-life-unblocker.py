b0f3442ad7c39f3ee569e4350b733dadd2ec9499
Tom Eastman <tom@eastman.nz>
2024-12-15 12:53:16 +1300

    Some improvements:
    - Store the block list and don't fetch the whole thing each
      time.
    - Run the unblocker after fetching the block list.
    - Use asyncio.TaskGroup for unblocking.
    - Use a context manager to load and save the state.

--- bsky-half-life-unblocker/bsky-half-life-unblocker.py
+++ bsky-half-life-unblocker/bsky-half-life-unblocker.py
@@ -4,25 +4,20 @@ import pydantic
 import pathlib
 import time
 import random
+import atproto
+import contextlib
 
 STATE_FILENAME = pathlib.Path.home() / ".bsky-half-life-unblocker-state.json"
 
 console = rich.console.Console()
 
-HALF_LIFE = 365 / 12 * 2
-THRESHOLD = 0.01
+HALF_LIFE = 365.25 / 3.0
+THRESHOLD = 0.00
 
-background_tasks = set()
 
 def daystamp():
     """The unix timestamp but in 'days' instead of seconds."""
     return time.time() / (60 * 60 * 24)
-
-
-def run_background_task(coro):
-    task = asyncio.create_task(coro)
-    background_tasks.add(task)
-    task.add_done_callback(background_tasks.discard)
 
 
 class State(pydantic.BaseModel):
@@ -30,6 +25,9 @@ class State(pydantic.BaseModel):
 
     session: str | None = None
     last_unblock_run_daystamp: float = pydantic.Field(default_factory=daystamp)
+    app_bsky_graph_block_list: dict[str, atproto.models.AppBskyGraphBlock.Record] = (
+        pydantic.Field(default_factory=dict)
+    )
 
     @classmethod
     def load(cls):
@@ -40,48 +38,74 @@ class State(pydantic.BaseModel):
             return cls()
 
     def save(self):
-        pathlib.Path(STATE_FILENAME).write_text(self.model_dump_json(indent=2))
+        start = time.time()
+        pathlib.Path(STATE_FILENAME).write_text(self.model_dump_json(indent=1))
+        duration = time.time() - start
+        console.log(f"saved state in {duration} seconds")
+
+    @classmethod
+    @contextlib.contextmanager
+    def auto_load_and_save(cls):
+        state = cls.load()
+        yield state
+        state.save()
 
 
 def keep_session_updated(client, state):
     """Update the state file whenever the session token is refreshed"""
 
     async def on_session_change(event, session):
-        console.log("saving updated session string")
         state.session = session.export()
-        state.save()
 
     client.on_session_change(on_session_change)
 
 
-async def fetch_all_block_records(client, probability):
-    import atproto
-    records = {}
+async def update_block_list(client, block_list):
     cursor = None
+    overlap = False
+    new_records = 0
     while True:
         console.log(f"fetching page of block records using cursor {cursor!r}")
         lrr = await client.app.bsky.graph.block.list(
             repo=client.me.did, cursor=cursor, limit=100
         )
-        records.update(lrr.records)
-
         for uri, record in lrr.records.items():
+            if uri in block_list:
+                overlap = True
+            else:
+                new_records += 1
+            block_list[uri] = record
+        cursor = lrr.cursor
+        if cursor is None:
+            console.log("last page fetched")
+            break
+        if overlap:
+            console.log("fetched all new records")
+            break
+    console.log(f"new block records: {new_records}")
+
+
+async def unblock(client, block_list, uri):
+    record = block_list[uri]
+    rkey = atproto.AtUri.from_str(uri).rkey
+    link = f"https://bsky.app/profile/{record.subject}"
+    result = await client.app.bsky.graph.block.delete(repo=client.me.did, rkey=rkey)
+    if result:
+        del block_list[uri]
+        console.print(f"unblocked [link={link}]{record.subject}[/link]")
+    else:
+        console.print(f"failed to unblock [link={link}]{record.subject}[/link]")
+
+
+async def randomly_unblock(client, block_list, probability):
+    async with asyncio.TaskGroup() as task_group:
+        for uri, record in list(block_list.items()):
             if random.random() >= probability:
                 # ...stays blocked.
                 continue
-
-            # trigger a background task to unblock this account
-            rkey = atproto.AtUri.from_str(uri).rkey
-            run_background_task(client.app.bsky.graph.block.delete(repo=client.me.did, rkey=rkey))
-            link = f"https://bsky.app/profile/{record.subject}"
-            console.print(f"Unblocking [link={link}]{record.subject}[/link]")
-
-        cursor = lrr.cursor
-        if cursor is None:
-            break
-           
-    console.log(f"fetched {len(records)} records")
-    return records
+            task_group.create_task(
+                unblock(client, block_list, uri), name=f"unblock {uri}"
+            )
 
 
 def calculate_decay_probability(time, half_life):
@@ -92,41 +116,35 @@ def calculate_decay_probability(time, half_life):
 
 
 async def main():
-    state = State.load()
+    with State.auto_load_and_save() as state:
+        current_daystamp = daystamp()
+        time_period = current_daystamp - state.last_unblock_run_daystamp
+        probability = calculate_decay_probability(time_period, HALF_LIFE)
+        console.log("Days since last run:", time_period)
+        console.log(f"Probability of unblocking per account: {probability:%}")
 
-    current_daystamp = daystamp()
-    time_period = current_daystamp - state.last_unblock_run_daystamp
-    probability = calculate_decay_probability(time_period, HALF_LIFE)
-    console.log("Days since last run:", time_period)
-    console.log(f"Probability of unblocking per account: {probability:%}")
+        threshold = THRESHOLD
+        if probability < threshold:
+            console.log(f"Waiting till the probability is greater than {threshold:%}")
+            return
 
-    threshold = THRESHOLD
-    if probability < threshold:
-        console.log(f"Waiting till the probability is greater than {threshold:%}")
-        return
+        client = atproto.AsyncClient()
+        keep_session_updated(client, state)
 
-    import atproto
-    client = atproto.AsyncClient()
-    keep_session_updated(client, state)
+        if state.session:
+            console.log("logging in using exported session string")
+            await client.login(session_string=state.session)
+        else:
+            console.log("logging in using application password")
+            username = input("Username: ")
+            password = input("app password: ")
+            await client.login(username, password)
 
-    if state.session:
-        console.log("logging in using exported session string")
-        await client.login(session_string=state.session)
-    else:
-        console.log("logging in using application password")
-        username = input("Username: ")
-        password = input("app password: ")
-        await client.login(username, password)
+        await update_block_list(client, state.app_bsky_graph_block_list)
+        state.last_unblock_run_daystamp = current_daystamp
+        await randomly_unblock(client, state.app_bsky_graph_block_list, probability)
 
-    await fetch_all_block_records(client, probability)
-
-
-    console.log("waiting for tasks to complete")
-    await asyncio.gather(*background_tasks)
-    console.log("all tasks completed")
-
-    state.last_unblock_run_daystamp = current_daystamp
-    state.save()
+        console.log(f"number of block records: {len(state.app_bsky_graph_block_list)}")
 
 
 asyncio.run(main(), debug=False)
